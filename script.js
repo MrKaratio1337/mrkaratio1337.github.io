98cf4ad2086bd604528a036764b36b53db5ea5d2
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-10-16 16:52:26 +0200

    Dodanie kalkulatora z dodawaniem

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ document.getElementById('DOMContentLoaded', function (){
     button.addEventListener('click', function(){
         alert('Kliknąłeś przycisk');
     });
+
+    const addButton = document.getElementById('addButton');
+    const num1Input = document.getElementById('num1');
+    const num2Input = document.getElementById('num2');
+    const resultParagraph = document.getElementById('result');
+
+    addButton.addEventListener('click', function () {
+        const num1 = parseFloat(num1Input.value);
+        const num2 = parseFloat(num2Input.value);
+        const sum = num1 + num2;
+        resultParagraph.textContent = 'Wynik: ' + sum;
+    });
 });
