7861f8d78c06ef6655f9c30f92289ec4f4f07f07
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-11-11 19:02:48 +0100

    obliczanie potęg dla kolegi

---
+++ js/dla-kacpra/script.js
@@ -0,0 +1,12 @@
+const wynik = document.getElementById('wynik');
+const pole = document.getElementById('podstawa');
+const wykladnik = document.getElementById('wykladnik');
+
+function check(){
+    var number = pole.value;
+    var number2 = wykladnik.value;
+
+    var oblicz = Math.pow(number, number2);
+
+    wynik.innerHTML = "Wynik: " + oblicz;
+}
