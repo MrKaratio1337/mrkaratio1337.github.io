1e3484a97ec5db564a2fcd6a6b3108a64c1b9ed7
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-12-01 15:29:11 +0100

    add zadanie 4

--- kolejne-zadania/zadanie3/script.js
+++ kolejne-zadania/zadanie3/script.js
@@ -4,9 +4,16 @@ function add(a, b){
     if(typeof(b) === "undefined") {
         b=1;
     }
-    //result.innerHTML = a + "+" + b + "=" + (a+b) + "<br>";
+    
+    if(arguments.length < 3) {
+        result.innerHTML = a + "+" + b + "=" + (a+b) + "<br>";
+    }
+    
+    else {
+        result.innerHTML = a + "+" + b + "+" + arguments[2] + "=" + (a+b+arguments[2]) + "<br>";
+    }
 
-    result.innerHTML = a + "+" + b + "+" + arguments[2] + "=" + (a+b+arguments[2]) + "<br>";
+    //result.innerHTML = a + "+" + b + "=" + (a+b) + "<br>";
 }
 
 add(10,20,9);
