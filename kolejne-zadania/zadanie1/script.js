e7e4566970b9dc943ba104578b7d94f56a420117
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-12-01 14:37:38 +0100

    update

--- kolejne-zadania/zadanie1/script.js
+++ kolejne-zadania/zadanie1/script.js
@@ -1,1 +1,4 @@
-document.getElementById('result').innerHTML = "Hello World";
+//document.getElementById('result').innerHTML = "Hello World!";
+
+var result = document.getElementById('result');
+result.innerHTML = "Hello World!";
