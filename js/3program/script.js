f27f53d19817c93bc1ddf05e1dc3b17e321ae3ad
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-12-15 14:15:14 +0100

    Delete js directory

--- js/3program/script.js
+++
@@ -1,8 +0,0 @@
-function check(){
-    var number = document.getElementById('pole').value;
-    if(number >= 10){
-        document.getElementById('wynik').innerHTML = (number * 2);
-    } else{
-        document.getElementById('wynik').innerHTML = 0;
-    }
-}
