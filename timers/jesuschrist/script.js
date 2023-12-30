e9b45dfe7067621c709d04088a9ae1afa7e60cb9
MrKaratio1337 <83728880+MrKaratio1337@users.noreply.github.com>
2023-12-30 19:54:25 +0100

    Add files via upload

--- timers/jesuschrist/script.js
+++ timers/jesuschrist/script.js
@@ -1,5 +1,5 @@
 function updateTimer(){
-    const targetDate = new Date("2023-12-24T00:00+01:00").getTime();
+    const targetDate = new Date("2024-12-24T00:00+01:00").getTime();
     const currentDate = new Date().getTime();
     const timeDifference = targetDate - currentDate;
 
